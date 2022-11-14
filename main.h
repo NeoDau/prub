862d233bd6f39e73bab4eb939f5a50e65f2893e0
NeoDau <neodau@gmail.com>
2022-11-13 16:37:40 -0800

    add the main.h and _putchar

--- main.h
+++ main.h
@@ -1,33 +1,35 @@
 #ifndef _PRINTF_
 #define _PRINTF_
 
+#include <limits.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <stdarg.h>
-#include "_putchar.c"
-#include "probando.c"
+#include <stddef.h>
+#include <stdlib.h>
 
 typedef struct caract
 {
 	char sim;
 	int (*f)(va_list);
-}type_ch;
+} type_ch;
 
 int _printf(const char *format, ...);
 
+int (*get_funct(char formatic))(va_list);
+
 int _putchar(char c);
 
-int c_formatic(va_list c);
+int c_formatic(va_list arg);
 
-int s_formatic(va_list s);
+int s_formatic(va_list arg);
 
-int d_formatic(va_list d);
+int d_formatic(va_list arg);
 
-int i_formatic(va_list d);
+int i_formatic(va_list arg);
 
-int porcent_formatic(va_list porcent);
-
-
+/**int porcent_formatic(va_list arg);*/
 
 
 #endif
